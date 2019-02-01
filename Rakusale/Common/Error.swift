4970879526ad6a2bb348036561a3bea3874bdb47
RyoTozawa <b1016031@fun.ac.jp>
2019-02-01 21:52:46 +0900

    [Fix] Directory Construction

---
+++ Rakusale/Common/Error.swift
@@ -0,0 +1,9 @@
+//
+//  Error.swift
+//  Rakusale
+//
+//  Created by 戸澤涼 on 2019/02/01.
+//  Copyright © 2019 Ryo.Tozawa. All rights reserved.
+//
+
+import Foundation
---
+++ Rakusale/Common/Lottie-Bridging-Header.h
@@ -0,0 +1,13 @@
+//
+//  Lottie-Bridging-Header.h
+//  Rakusale
+//
+//  Created by 戸澤涼 on 2019/02/01.
+//  Copyright © 2019 Ryo.Tozawa. All rights reserved.
+//
+
+#ifndef Lottie_Bridging_Header_h
+#define Lottie_Bridging_Header_h
+
+
+#endif /* Lottie_Bridging_Header_h */
