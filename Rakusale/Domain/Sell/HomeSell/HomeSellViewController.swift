4d5a7d4cade73e5237249f7ffcf6f253e553d523
RyoTozawa <b1016031@fun.ac.jp>
2019-02-03 22:15:40 +0900

    [Add] VegaScroll

--- Rakusale/Domain/Sell/HomeSell/HomeSellViewController.swift
+++ Rakusale/Domain/Sell/HomeSell/HomeSellViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 import Alamofire
 import AlamofireImage
 import PromiseKit
+import VegaScrollFlowLayout
 
 class HomeSellViewController: UIViewController, UICollectionViewDataSource {
     
@@ -18,7 +19,7 @@ class HomeSellViewController: UIViewController, UICollectionViewDataSource {
     fileprivate let refreshCtl = UIRefreshControl()
     let imageCache = AutoPurgingImageCache()
     let imageNotFound = UIImage(named: "404")
-    
+    let layout = VegaScrollFlowLayout()
     let waitTime: Double = 2.0
     let alert: UIAlertController = UIAlertController(title: "Invaild Login", message: "Please Retype", preferredStyle:  .alert)
     
@@ -38,6 +39,10 @@ class HomeSellViewController: UIViewController, UICollectionViewDataSource {
         self.navigationController?.isNavigationBarHidden = false
         
         self.uiCollectionView.delegate = self
+        uiCollectionView.collectionViewLayout = layout
+        layout.minimumLineSpacing = 20
+        layout.itemSize = CGSize(width: uiCollectionView.frame.width, height: 80)
+        layout.sectionInset = UIEdgeInsets(top: 5, left: 0, bottom: 5, right: 0)
         // 画面引っ張ったらくるくる回って更新するためのインスタンス
         self.uiCollectionView.refreshControl = refreshCtl
         refreshCtl.addTarget(self, action: #selector(HomeSellViewController.refresh(sender:)), for: .valueChanged)
@@ -62,6 +67,9 @@ class HomeSellViewController: UIViewController, UICollectionViewDataSource {
             withReuseIdentifier: "Cell",
             for: indexPath
         )
+//        cell.layer.borderColor = UIColor.black.cgColor
+//        cell.layer.cornerRadius = 30
+//        cell.layer.masksToBounds = true
         // 画像データ取得
         let imageView = cell.contentView.viewWithTag(1) as! UIImageView
         let url: String = vegetable.imagePath
@@ -145,7 +153,7 @@ class HomeSellViewController: UIViewController, UICollectionViewDataSource {
 
 extension HomeSellViewController: UICollectionViewDelegateFlowLayout {
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        return CGSize(width: (collectionView.bounds.width - 13) / 2, height: (collectionView.bounds.width - 13) / 2)
+        return CGSize(width: (collectionView.bounds.width - 13), height: (collectionView.bounds.width) / 2 )
     }
 }
 
